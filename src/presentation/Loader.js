c4fbd905d5762a51fe8b52eec437f3c72d9ad68b
bartekma89 <bartekma89@gmail.com>
2018-06-18 05:53:59 +0200

    added loader scss

--- src/presentation/Loader.js
+++ src/presentation/Loader.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Loader.css';
 
 const Loader = ({ hasErrored, isLoading, errorMessage, children }) => {
 	if (hasErrored) {
